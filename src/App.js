0a6fcfef804e63534ead64f11f745eea730e0dc0
acharyaraksha84 <shrirakshapacharya@gmail.com>
2025-04-29 15:35:34 +0530

    Resolved merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 // src/App.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom'; // Correct imports
 import LoginPage from './pages/LoginPage';
 import HODDashboard from './pages/HODDashboard';
 import TeacherView from './pages/TeacherView';
 import StudentView from './pages/StudentView';
-<<<<<<< HEAD
-import Classes from './pages/Classes'; 
-import ViewTimetable from './pages/ViewTimetable';
-=======
 import FacultyPage from './pages/FacultyPage';
->>>>>>> 37630ca30392ebfa71c324a19daa787e93d1475f
 
 const App = () => {
   return (
@@ -21,12 +15,7 @@ const App = () => {
         <Route path="/hod" element={<HODDashboard />} />
         <Route path="/teacher" element={<TeacherView />} />
         <Route path="/student" element={<StudentView />} />
-<<<<<<< HEAD
-        <Route path="/classes" element={<Classes />} />
-        <Route path="/classes/:className" element={<ViewTimetable />} />
-=======
         <Route path="/faculty" element={<FacultyPage />} />
->>>>>>> 37630ca30392ebfa71c324a19daa787e93d1475f
       </Routes>
     </div>
   );
